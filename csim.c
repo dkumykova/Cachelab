c45653f55a853002a5f21aef55497155ecd5932a
Diana Kumykova <dkumykova@gmail.com>
2018-12-13 08:48:11 -0500

    reformatted and cleaned up

--- csim.c
+++ csim.c
@@ -5,7 +5,6 @@
 #include <string.h>
 
 //dkumykova-tgsprowl
-//create struct for cachelines, typedef
 
 typedef struct cacheLine {
 	int validBit;
@@ -17,13 +16,14 @@ typedef struct cacheLine {
 } cacheLine;
 
 typedef struct cacheSet {
-	cacheLine *lines; //pointer to lines in set
+	cacheLine *lines;
 } cacheSet;
+
 typedef struct cache {
-	cacheSet *sets; //pointer to sets in a cache
+	cacheSet *sets;
 
 } cache;
-
+//for line read in from trace file
 typedef struct cacheTrace {
 	char* instruction;
 	unsigned long memAddress;
@@ -35,16 +35,20 @@ int hits = 0;
 int misses = 0;
 int evictions = 0;
 
-cache make2Dcache(int nsets, int nlines); //function def
-void printInstructions(); //prints out help sintructions
-void printInstruct(FILE* input, cacheTrace t); //reads in the instructions
+cache make2Dcache(int nsets, int nlines); //creates and allocates space for new cache
+void printInstructions(); //prints out help instructions
+void printInstruct(FILE* input, cacheTrace t); //prints the line from trace file
 long calcTag(unsigned long mem_address, int blocksize, int setIndexBits); //calculates tag of a given cache line
 int calcSetIndex(long tag, int blocksize, int setIndexBits); //calculates set index of a cache line
-int matchLine(cache *test, long tag, int setIndex, int nlines); //checks to see if line is within a cache
-int findEmptyLine(cache *test, int setIndex, int nlines);
+int matchLine(cache *test, long tag, int setIndex, int nlines); //returns the index of a line if it is within a cache
+int findEmptyLine(cache *test, int setIndex, int nlines); //returns the index of an emtpy line or the least recently
+//used line
+void runSim(cache test, cacheLine parsedLine, long tag, int numLines,
+		int loopCounter, int i, int index);
 
 int main(int argc, char* argv[]) {
-	//takes as input number of sets, lines per set, and number of block bits
+	//takes as input number of sets, lines per set, number of block bits, number of set index bits,
+	//and filename
 	int help = 0;
 	int verbose = 0;
 	int setIndexBits = 0;
@@ -96,47 +100,36 @@ int main(int argc, char* argv[]) {
 	T1.memAddress = 0L;
 	T1.instructSize = (int*) malloc(sizeof(int));
 
-	//dimensions for cache, lineIndexInCache from inputs
-	//bits is b, blocksize is 2^b
-	//set index is S = 2^s
+	//dimensions for cache
 	int numSets = 2 << setIndexBits;
 	int blockSize = 2 << blockBits;
-	//int tag = memAddress % blocksize; //remainder after take away block size
-	//how to calculate? get rid of block bits at end, so shift right by blocksize; then
-	//shift left by tag; that should isolate the set Index
 
 	//create cache with given dimensions
 	cache test;
 	test = make2Dcache(numSets, numLines);
 
-//-------------------------------------------------------------------------------------------------
-
 	//open and parse given trace file
 	FILE *fp;
-
-	/* opening file for reading */
 	fp = fopen(fileName, "r");
 	if (fp == NULL) {
 		perror("Error opening file");
 		return (-1);
 	}
 	int loopCounter = 0; //keeps track of number of loops function goes through to act as clock for number of times
-	//each line is accessed
+						 //each line is accessed
+
 	//loops through file until finished, performs operation on each line; line is printed
 	while ((fscanf(fp, " %c %lx,%d\n", T1.instruction, &T1.memAddress,
 			T1.instructSize)) != EOF) {
 		loopCounter++;
-		printf("\nThis is the first memAddress: %ld\n", T1.memAddress);
-		//long final_address = strtol(*T1.memAddress, NULL, 16);
+		printf("\nmemAddress: %ld\n", T1.memAddress);
 
-		//need to convert the memAddress to actual correct value, right now it's just a hex number and shifting it over does nothing?
-		//convert to base 16?
-
-		long tag = calcTag(T1.memAddress, blockSize, setIndexBits); //need to get it so references mem address correctly
-		printf("This is the tag: %ld\n", tag);
+		long tag = calcTag(T1.memAddress, blockSize, setIndexBits);
 		long index = calcSetIndex(T1.memAddress, blockBits, setIndexBits);
-		printf("This is the index: %ld\n", index);
+		printf("Tag: %ld\n", tag);
+		printf("Index: %ld\n", index);
 		printInstruct(fp, T1);
+
 		//create line from instruction read
 		cacheLine parsedLine;
 		parsedLine.validBit = 0;
@@ -146,136 +139,114 @@ int main(int argc, char* argv[]) {
 		parsedLine.times_accessed = 0; //assign number of times the line is accessed to number of times the loop has parsed that line
 
 		int i = 0; //index for lines array
-		//PUT ALL THE STUFF FOR EACH INSTRUCTION CASE INTO SEPARATE FUNCTION FOR EASE OF READING LATER
-		//THE CURRENT SETUP CALLS M TOO MANY TIMES, MAKE INTO A SWITCH STATEMENT
 
 		if (strcmp(T1.instruction, "L") == 0
-				|| (strcmp(T1.instruction, "M")) == 0) { //load, finds address of what looking for
-			loopCounter++;
-			int lineIndexInCache = matchLine(&test, tag, index, numLines); //tries to find given line in the cache by returning index
-
-
-			if (lineIndexInCache == -1) { //miss
-				int lineToReplaceIndex = findEmptyLine(&test, index, numLines); //returns index of empty line
-				//need to find empty before place anything within it; place in first empty line encountered; spit out index for empty line?
-
-				//lineToReplaceIndex = parsedLine; //place line within the cache at specific set index, change valid bit to 1
-				test.sets[index].lines[lineToReplaceIndex].blockSize =
-						parsedLine.blockSize; //place the value at index lineIndexInCache into value at index lineToReplaceIndex
-				test.sets[index].lines[lineToReplaceIndex].sIndex =
-						parsedLine.sIndex;
-				test.sets[index].lines[lineToReplaceIndex].tag = parsedLine.tag;
-				test.sets[index].lines[lineToReplaceIndex].times_accessed =
-						loopCounter;
-				test.sets[index].lines[lineToReplaceIndex].validBit = 1;
-				//parsedLine.validBit = 1;
-				printf("This is a miss.\n");
-				misses++; //increment miss counter, times accessed updated in find empty line
-				if (i == numLines) {
-					i = 0;
-				} else {
-					i++; //go to next line within array of lines for parsing
-				}
-			} else { //hit
-				printf("This is a hit.\n");
-				test.sets[index].lines[lineIndexInCache].times_accessed = loopCounter; //update times_accessed
-				hits++;
-				//increment hit counter
-
-			}
+				|| (strcmp(T1.instruction, "M")) == 0) { //load, finds address of what looking for; including M makes sure M is done twice when called
+			runSim(test, parsedLine, tag, numLines, loopCounter, i, index);
 
 		}
 		if ((strcmp(T1.instruction, "S")) == 0
 				|| (strcmp(T1.instruction, "M")) == 0) { //store, similar to load
-			loopCounter++;
-			int lineIndexInCache = matchLine(&test, tag, index, numLines); //tries to find given line in the cache by returning index
+			runSim(test, parsedLine, tag, numLines, loopCounter, i, index);
+		}
+	} //otherwise it's I, which we do nothing with
 
-
-			if (lineIndexInCache == -1) { //miss
-				int lineToReplaceIndex = findEmptyLine(&test, index, numLines); //returns index of empty line
-				//need to find empty before place anything within it; place in first empty line encountered; spit out index for empty line?
-
-				//lineToReplaceIndex = parsedLine; //place line within the cache at specific set index, change valid bit to 1
-				test.sets[index].lines[lineToReplaceIndex].blockSize =
-						parsedLine.blockSize; //place the value at index lineIndexInCache into value at index lineToReplaceIndex
-				test.sets[index].lines[lineToReplaceIndex].sIndex =
-						parsedLine.sIndex;
-				test.sets[index].lines[lineToReplaceIndex].tag = parsedLine.tag;
-				test.sets[index].lines[lineToReplaceIndex].times_accessed =
-						loopCounter;
-				test.sets[index].lines[lineToReplaceIndex].validBit = 1;
-				//parsedLine.validBit = 1;
-				printf("This is a miss.\n");
-				misses++; //increment miss counter, times accessed updated in find empty line
-				if (i == numLines) {
-					i = 0;
-				} else {
-					i++; //go to next line within array of lines for parsing
-				}
-			} else { //hit
-				printf("This is a hit.\n");
-				test.sets[index].lines[lineIndexInCache].times_accessed = loopCounter; //update times_accessed
-				hits++;
-				//increment hit counter
-
-			}
-		} //otherwise it's I, which we do nothing with
-
-	}
 	fclose(fp);
 
 	printSummary(hits, misses, evictions);
 	return 0;
 }
 
+/**Runs the cache simulator
+ * @param test, cache being searched and loaded into
+ * @param parsedLine, line being searched for, given by trace file instruction
+ * @apram tag, tag for given line
+ * @param numLines, number of lines in each set
+ * @param loopCounter, counts number of iterations through this loop
+ * @param i, iterator to go through lines
+ * @param index, set index for given line
+ *
+ */
+void runSim(cache test, cacheLine parsedLine, long tag, int numLines,
+		int loopCounter, int i, int index) {
+	loopCounter++;
+	int lineIndexInCache = matchLine(&test, tag, index, numLines); //tries to find given line in the cache by returning index
+
+	if (lineIndexInCache == -1) { //miss
+		int lineToReplaceIndex = findEmptyLine(&test, index, numLines); //returns index of empty line
+
+		//place the value at index lineToReplaceIndex to the parsedLine values given from the read in address
+		test.sets[index].lines[lineToReplaceIndex].blockSize =
+				parsedLine.blockSize;
+		test.sets[index].lines[lineToReplaceIndex].sIndex = parsedLine.sIndex;
+		test.sets[index].lines[lineToReplaceIndex].tag = parsedLine.tag;
+		test.sets[index].lines[lineToReplaceIndex].times_accessed = loopCounter;
+		test.sets[index].lines[lineToReplaceIndex].validBit = 1;
+
+		printf("Miss.\n");
+		misses++;
+		if (i == numLines) { //iterate through all lines in set
+			i = 0;
+		} else {
+			i++; //go to next line within array of lines for parsing
+		}
+	} else { //hit
+		printf("Hit.\n");
+		test.sets[index].lines[lineIndexInCache].times_accessed = loopCounter; //update times_accessed to equals number of loops
+		hits++;
+
+	}
+}
+
 //need to check if line is empty before place anything within it; place in first empty line encountered; spit out index for empty line?
+/**Returns the index of an empty line, or if the cache is full, the line
+ * to be evicted when putting a new line into the cache.
+ * @param test, cache in which we are looking for a line to replace
+ * @param setIndex, details which specific set we are looking for a line
+ * @param nlines, number of lines in that set
+ * @return the index of the line in the set to be replaced
+ */
 int findEmptyLine(cache *test, int setIndex, int nlines) {
 	int last_used = test->sets[setIndex].lines[0].times_accessed; //keep track of line accessed least amount
 
-	int index = 0; //return value
+	int index = 0; //return value, initialized
 
-	int i; //iterate
+	int i; //iterator
 	for (i = 0; i < nlines; i++) {
 		if (test->sets[setIndex].lines[i].validBit == 0) { //this is an empty line
-			index = i;
+			index = i; //assign and return index of empty line immediately
 			return index;
-		} else { //this means the line is full
+		} else { //this means the set is full, because no empty line has been found
 
 			if (last_used > test->sets[setIndex].lines[i].times_accessed) {
 				last_used = test->sets[setIndex].lines[i].times_accessed;
-				printf("This is the number of times accessed: %d\n", last_used);
 				index = i;
-
-
 			}
-
 		}
-
 	}
-	//handle eviction case, because iterated through lines and no empty one; take one with
-	//clear line by setting valid bit to 0
+//handle eviction case, because iterated through lines and no empty one;
+//clear line by setting valid bit to 0
 	test->sets[setIndex].lines[index].validBit = 0;
 	evictions++;
-	printf("This is an eviction.\n");
+	printf("Eviction.\n");
 
 	return index; //return index of the empty line within the cache
 }
-/**Returns 1 if the line matches the one described, else return 0
- *@param test, cache which we are looking in for the given line
+/**Returns the index of the line if it is located within the cache; if not,
+ * returns -1
+ *@param test, cache in which we are looking for the given line
  *@param tag, tag bits for given line
  *@param setIndex, set index for given line
- *@param blocksize, block offset for given line
  *@param nline, number of lines in cache
- *@param nsets, number of sets in cache
- *@return 0 for miss, 1 for hit, 2 for eviction
+ *@return -1 as index if line is not within the cache; otherwise returns index of line in cache
  */
 int matchLine(cache *test, long tag, int setIndex, int nlines) {
-	//different logic
+//different logic
 	cacheSet set = test->sets[setIndex];
 	int index = -1;
 
 	for (int i = 0; i < nlines; i++) {
-		if (set.lines[i].tag == tag && set.lines[i].validBit == 1) {
+		if (set.lines[i].tag == tag && set.lines[i].validBit == 1) { //tag matches and bit is valid
 			index = i;
 		}
 
@@ -286,15 +257,15 @@ int matchLine(cache *test, long tag, int setIndex, int nlines) {
 
 /**Calculates the set index of a line
  * @param mem_address, the given address in memory for the line being evaluated
- * @param blocksize, size of block offset
- * @param setIndexBits, number of set index bits
+ * @param blockBits, number of block bits in line (read in from argv)
+ * @param setIndexBits, number of set index bits (read in from argv)
  * @return the set index
  */
 int calcSetIndex(long mem_address, int blockBits, int setIndexBits) {
 	printf("CALC SI addr %ld  b %d s %d", mem_address, blockBits, setIndexBits);
 
-	long tag_and_s_index = mem_address >> blockBits; //shift line over right blocksize bits to get rid of the block bits at the end
-	long stuff = tag_and_s_index >> setIndexBits;
+	long tag_and_s_index = mem_address >> blockBits; //shift line over right # block bits to get rid of the block bits at the end
+	long stuff = tag_and_s_index >> setIndexBits; //account for signed mem address
 	long index = tag_and_s_index ^ (stuff << setIndexBits); //gets rid of garbage bits
 
 	return index;
@@ -304,7 +275,7 @@ int calcSetIndex(long mem_address, int blockBits, int setIndexBits) {
  * @param mem_address, the given address in memory for the line being evaluated
  * @param blocksize, size of block offset
  * @param setIndexBits, number of set index bits
- * @return the tag]
+ * @return the tag
  */
 long calcTag(unsigned long mem_address, int blocksize, int setIndexBits) {
 
@@ -324,29 +295,24 @@ cache make2Dcache(int nsets, int nlines) {
 	cache testCache; // Array of pointers to rows
 	int sIndex;
 	int lIndex;
-	//allocate array of pointers to rows
-	testCache.sets = (cacheSet*) malloc(nsets * sizeof(cacheSet)); //put sets into cache
+//allocate array of pointers to rows
+	testCache.sets = (cacheSet*) malloc(nsets * sizeof(cacheSet)); //space for sets
 	for (sIndex = 0; sIndex < nsets; sIndex++) {
-		testCache.sets[sIndex].lines = (cacheLine*) malloc(nlines * sizeof(cacheLine));
-		//assign value of newSet to
-		//corresponding index in the cache created
+		testCache.sets[sIndex].lines = (cacheLine*) malloc(
+				nlines * sizeof(cacheLine)); //space for lines
 
 		//initialize everything in the lines to 0, clean cache
 		for (lIndex = 0; lIndex < nlines; lIndex++) {
 			testCache.sets[sIndex].lines[lIndex].times_accessed = 0;
 			testCache.sets[sIndex].lines[lIndex].validBit = 0;
 			testCache.sets[sIndex].lines[lIndex].tag = 0;
-			testCache.sets[sIndex].lines[lIndex].blockSize= 0;
+			testCache.sets[sIndex].lines[lIndex].blockSize = 0;
 			testCache.sets[sIndex].lines[lIndex].sIndex = sIndex;
-			//assign value of newLine to
-			//corresponding address/index in set
 		}
 	}
 
 	return testCache;
 }
-//now that we've intialized the cache we need to be able to read things into it,
-//clear things from it, and evict lines and replace with new one
 
 /**Prints out help statements if the -h flag is called
  * @param argv, the input arguments given my user
